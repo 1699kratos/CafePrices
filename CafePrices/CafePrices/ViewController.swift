8638de6230d94317cbea7360e8373f9ef784618c
1699kratos <erickhdzrdz@hotmail.com>
2021-10-21 16:12:39 -0700

    Empty proyect

---
+++ CafePrices/CafePrices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CafePrices
+//
+//  Created by erick eduardo on 21/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CafePrices/CafePricesTests/CafePricesTests.swift
@@ -0,0 +1,33 @@
+//
+//  CafePricesTests.swift
+//  CafePricesTests
+//
+//  Created by erick eduardo on 21/10/21.
+//
+
+import XCTest
+@testable import CafePrices
+
+class CafePricesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
