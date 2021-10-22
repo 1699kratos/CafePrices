ebcbdc1f9af8551caac8ae63fd5e9ce627d98428
1699kratos <erickhdzrdz@hotmail.com>
2021-10-22 16:46:08 -0700

    Deleted the buttom from the tableView and fix code

--- CafePrices/CafePrices/TableViewCell.swift
+++ CafePrices/CafePrices/TableViewCell.swift
@@ -8,17 +8,13 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-    
-    @IBOutlet weak var cafeteriaButton: UIButton!
     @IBOutlet weak var cafeteriaImage: UIImageView!
     @IBOutlet weak var cafeteriaName: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        
     }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
+
+    override func setSelected(_ selected: Bool, animated: Bool ) {
         super.setSelected(selected, animated: animated)
     }
-    
 }
